4e1efbf9f99ad79e9335f18f8d2c98195f1ba00b
pankaj sengar <pks11iitr@gmail.com>
2020-09-02 11:14:31 +0530

    contact message api

--- app/Http/Controllers/Customer/Api/SearchController.php
+++ app/Http/Controllers/Customer/Api/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Customer\Api;
 
+use App\Models\Cart;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -11,9 +12,19 @@ class SearchController extends Controller
     public function index(Request $request){
         $search=$request->search;
 
+        $user=auth()->guard('customerapi')->user();
+
         $products=Product::active()
             ->where('name', 'like', '%'.$search.'%')
             ->get();
+
+        $product_cart=Cart::getUserCart($user);
+        foreach($products as $i=>$r)
+        {
+
+            //$cart=Cart::where('user_id', auth()->guard('customerapi')->user()->id??'')->where('product_id',$r['id'])->get();
+            $products[$i]['qty']=$product_cart[$r->id]??0;
+        }
 
         return [
             'status'=>'success',
