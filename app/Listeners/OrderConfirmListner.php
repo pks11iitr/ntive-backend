182eaebd529f3cf979d388c24660676b62d3c56f
pankaj sengar <pks11iitr@gmail.com>
2020-09-07 19:15:38 +0530

    message change in notification

--- app/Listeners/OrderConfirmListner.php
+++ app/Listeners/OrderConfirmListner.php
@@ -40,9 +40,9 @@ class OrderConfirmListner
 
         $message='';
         if($order->details[0]->entity_type == 'App\Models\Product'){
-            $message='Congratulations! Your purchase of Rs. '.$order->total_cost.' at Arogyapeeth.com is successfull. Order Reference ID: '.$order->refid;
+            $message='Congratulations! Your purchase of Rs. '.$order->total_cost.' at Nitve Ecommerce is successfull. Order Reference ID: '.$order->refid;
         }else{
-            $message='Congratulations! Your therapy booking of Rs. '.$order->total_cost.' at Arogyapeeth.com is successfull. Order Reference ID: '.$order->refid;
+            $message='Congratulations! Your therapy booking of Rs. '.$order->total_cost.' at Nitve Ecommerce is successfull. Order Reference ID: '.$order->refid;
 
         }
 
