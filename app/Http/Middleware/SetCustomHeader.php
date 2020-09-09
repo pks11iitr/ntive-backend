097295e631728f58419a7a1542c69540a011768c
pankaj sengar <pks11iitr@gmail.com>
2020-09-09 23:16:44 +0530

    banner edit

--- app/Http/Middleware/SetCustomHeader.php
+++ app/Http/Middleware/SetCustomHeader.php
@@ -17,7 +17,7 @@ class SetCustomHeader
     public function handle($request, Closure $next)
     {
         $response =$next($request);
-        $response->header('Content-Security-Policy', "default-src 'self' 'unsafe-eval' 'unsafe-inline' *.nitve-ecommerce.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self' *.nitve-ecommerce.com data:; connect-src 'self' *.nitve-ecommerce.com; font-src 'self' *.nitve-ecommerce.com report-uri *.nitve-ecommerce.com/csp_report; form-action 'self'");
+        //$response->header('Content-Security-Policy', "default-src 'self' 'unsafe-eval' 'unsafe-inline' *.nitve-ecommerce.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self' *.nitve-ecommerce.com data:; connect-src 'self' *.nitve-ecommerce.com; font-src 'self' *.nitve-ecommerce.com report-uri *.nitve-ecommerce.com/csp_report; form-action 'self'");
         $response->header('Cache-Control', 'max-age=31536000');
 
 //        if(!starts_with(request()->path(), 'api') && !in_array($response->getStatusCode(), ['404'])){
