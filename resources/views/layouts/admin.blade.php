aadb6631e3585f1f620fb72e6abad44a87404d5d
pankaj sengar <pks11iitr@gmail.com>
2020-09-09 13:30:29 +0530

    change payment status

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <title>Ntive Dashboard</title>
+    <title>Nitve Dashboard</title>
 
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{asset('admin-theme/plugins/fontawesome-free/css/all.min.css')}}">
@@ -143,10 +143,10 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
+        <a href="{{route('home')}}" class="brand-link">
             <img src="{{asset('admin-theme/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
-            <span class="brand-text font-weight-light">Ntive CMS</span>
+            <span class="brand-text font-weight-light">Nitve CMS</span>
         </a>
 
         <!-- Sidebar -->
