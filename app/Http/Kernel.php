93ddd02aebb90555f9393e073448948315e667d8
pankaj sengar <pks11iitr@gmail.com>
2020-09-09 16:46:35 +0530

    add out of stock notification

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Bepsvpt\SecureHeaders\SecureHeadersMiddleware::class,
-        SetCustomHeader::class
     ];
 
     /**
@@ -38,6 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            SetCustomHeader::class
         ],
 
         'api' => [
